8363eb769cfee27cc6099f69a5cc9217c00b7abb
Vivek Patil <vvp8212@rit.edu>
2018-03-30 03:21:06 -0400

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { PromotionService } from './services/promotion.service';
 
 @NgModule({
   declarations: [
@@ -43,7 +44,7 @@ import { ContactComponent } from './contact/contact.component';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [ DishService ],
+  providers: [ DishService, PromotionService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/promotion.ts
+++ src/app/shared/promotion.ts
@@ -1,13 +1,9 @@
-import { Comment } from './comment';
-
-export class Dish {
-	id: number;
+export class Promotion {
+    id: number;
     name: string;
     image: string;
-    category: string;
     label: string;
     price: string;
     featured: boolean;
     description: string;
-    comments: Comment[];
 }
